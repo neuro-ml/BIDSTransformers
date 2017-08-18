4206857999d9c04a01aac47ccb5aaf4dfe460e42
nsdfyd <alexander.radievich@gmail.com>
2017-08-18 11:07:50 +0300

    tests for skull-stripping transformer

--- bidst/tests/test_transformers.py
+++ bidst/tests/test_transformers.py
@@ -4,12 +4,19 @@ import numpy as np
 sys.path.insert(0, os.path.abspath('../..'))
 
 import pytest
-from bids.grabbids import BIDSLayout
-from bidst.transformers import ROITransformer
-from bidst.transformers import SkullStrippingTransformer
 
-from nipype.interfaces.fsl import ExtractROI
+import bidst
+from bidst.path_manager import PathManager
+from bidst.transformers import NUCorrectionTransformer
+from bidst.transformers import SkullStrippingTransformer
+from bidst.transformers import LinearRegistrationTransformer
+from bidst.transformers import NonLinearRegistrationTransformer
+
 from nipype.interfaces.fsl import BET
+from nipype.interfaces.fsl import FLIRT
+from nipype.interfaces.fsl import FNIRT
+from nipype.interfaces.ants import N4BiasFieldCorrection
+
 import nibabel as nib
 
 from sklearn.pipeline import Pipeline
@@ -17,103 +24,91 @@ from nipype import Node, Workflow
 from shutil import rmtree
 
 
-def get_file_paths_with_sessions():
-    project_root = os.path.abspath('.') + '/ds114'
-    layout = BIDSLayout(project_root)
-    files = layout.get(extensions='.nii.gz', session='retest')
-    file_paths = [f.filename for f in files]
-    return file_paths
+def test_skull_stripping_tuples():
 
+    data_dir = os.path.abspath('.') + '/data/ds114'
+    pipeline_name = 'test_ROITransformer_ds114'
 
-def get_file_paths_without_sessions():
-    project_root = os.path.abspath('.') + '/ds114_without_sessions'
-    layout = BIDSLayout(project_root)
-    files = layout.get(extensions='.nii.gz')
-    file_paths = [f.filename for f in files]
-    return file_paths
+    IDS = [('01', 'retest'), 
+           ('02', 'retest'), 
+           ('03', 'test')]
 
+    transformer = SkullStrippingTransformer(data_dir=data_dir,
+                                            pipeline_name=pipeline_name,
+                                            search_param=dict(extensions='T1w.nii.gz'))
+    transformer.fit_transform(IDS)
 
-def cut_nii_gz(filename):
-    filename = os.path.splitext(filename)[0]
-    filename = os.path.splitext(filename)[0]
-    return filename
+    PM = PathManager(data_dir)
 
+    for subject, session in IDS:
 
-def add_nii_gz(filename):
-    return filename + '.nii.gz'
+        in_file = PM.get(extensions='T1w.nii.gz',
+                         subject=subject,
+                         session=session)
 
+        assert len(in_file) == 1
+        in_file = in_file[0]
 
-def test_ROITrasformer_with_sessions():
-    file_paths = get_file_paths_with_sessions()
-    basedir = os.path.abspath('.') + '/ds114'
-    result_paths = ROITransformer(label='label', t_min=0, t_size=1, basedir=basedir).fit_transform(file_paths)
-    for in_file, result_path in zip(file_paths, result_paths):
-        roi_file = add_nii_gz(cut_nii_gz(result_path) + '_needed')
-        fslroi = ExtractROI(in_file=in_file,
-                            roi_file=roi_file,
-                            t_min=0,
-                            t_size=1)
-        fslroi.run()
-        result_mat = nib.load(result_path).affine
-        output_mat = nib.load(roi_file).affine
+        out_file = PM.make(in_file=in_file,
+                           pipeline_name=pipeline_name,
+                           derivative='needed_brain')
+        dirname = os.path.dirname(out_file)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        betfsl = BET(in_file=in_file,
+                     out_file=out_file)
+        betfsl.run()
+
+        res_file = PM.make(in_file=in_file,
+                           pipeline_name=pipeline_name,
+                           derivative='brain')
+
+        result_mat = nib.load(res_file).affine
+        output_mat = nib.load(out_file).affine
         assert np.allclose(result_mat, output_mat)
-        os.remove(result_path)
-        os.remove(roi_file)
-        print(result_path)
-        print(roi_file)
+        os.remove(out_file)
+        os.remove(res_file)
 
 
-def test_ROITransformer_without_sessions():
-    file_paths = get_file_paths_without_sessions()
-    basedir = os.path.abspath('.') + '/ds114_without_sessions'
-    result_paths = ROITransformer(label='label', t_min=0, t_size=1, basedir=basedir).fit_transform(file_paths)
-    for in_file, result_path in zip(file_paths, result_paths):
-        roi_file = add_nii_gz(cut_nii_gz(result_path) + '_needed')
-        fslroi = ExtractROI(in_file=in_file,
-                            roi_file=roi_file,
-                            t_min=0,
-                            t_size=1)
-        fslroi.run()
-        result_mat = nib.load(result_path).affine
-        output_mat = nib.load(roi_file).affine
-        assert np.allclose(result_mat, output_mat)
-        os.remove(result_path)
-        os.remove(roi_file)
-        print(result_path)
-        print(roi_file)
+def test_skull_stripping_ids_only():
 
+    data_dir = os.path.abspath('.') + '/data/ds114'
+    pipeline_name = 'test_ROITransformer_ds114'
 
-def test_SkullStrippingTransformer_with_sessions():
-    file_paths = get_file_paths_with_sessions()
-    basedir = os.path.abspath('.') + '/ds114'
-    result_paths = SkullStrippingTransformer(basedir=basedir).fit_transform(file_paths)
-    for in_file, result_path in zip(file_paths, result_paths):
-        bet_file = add_nii_gz(cut_nii_gz(result_path) + '_needed')
-        fslbet = BET(in_file=in_file,
-                     out_file=bet_file)
-        fslbet.run()
-        result_mat = nib.load(result_path).affine
-        output_mat = nib.load(bet_file).affine
-        assert np.allclose(result_mat, output_mat)
-        os.remove(result_path)
-        os.remove(bet_file)
-        print(result_path)
-        print(bet_file)
+    IDS = ['01', '02', '03']
 
+    transformer = SkullStrippingTransformer(data_dir=data_dir,
+                                            pipeline_name=pipeline_name,
+                                            search_param=dict(extensions='T1w.nii.gz'))
+    transformer.fit_transform(IDS)
 
-def test_SkullStrippingTransformer_without_sessions():
-    file_paths = get_file_paths_without_sessions()
-    basedir = os.path.abspath('.') + '/ds114_without_sessions'
-    result_paths = SkullStrippingTransformer(basedir=basedir).fit_transform(file_paths)
-    for in_file, result_path in zip(file_paths, result_paths):
-        bet_file = add_nii_gz(cut_nii_gz(result_path) + '_needed')
-        fslbet = BET(in_file=in_file,
-                     out_file=bet_file)
-        fslbet.run()
-        result_mat = nib.load(result_path).affine
-        output_mat = nib.load(bet_file).affine
-        assert np.allclose(result_mat, output_mat)
-        os.remove(result_path)
-        os.remove(bet_file)
-        print(result_path)
-        print(bet_file)
+    PM = PathManager(data_dir)
+
+    for subject in IDS:
+
+        in_files = PM.get(extensions='T1w.nii.gz',
+                         subject=subject)
+        
+        for in_file in in_files:
+
+            out_file = PM.make(in_file=in_file,
+                               pipeline_name=pipeline_name,
+                               derivative='needed_brain')
+            dirname = os.path.dirname(out_file)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            betfsl = BET(in_file=in_file,
+                         out_file=out_file)
+            betfsl.run()
+
+            res_file = PM.make(in_file=in_file,
+                               pipeline_name=pipeline_name,
+                               derivative='brain')
+
+            result_mat = nib.load(res_file).affine
+            output_mat = nib.load(out_file).affine
+            assert np.allclose(result_mat, output_mat)
+            os.remove(out_file)
+            os.remove(res_file)
