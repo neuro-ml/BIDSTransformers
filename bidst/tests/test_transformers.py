e57b524a148926ce302bee77dab433d5a386e628
nsdfyd <alexander.radievich@gmail.com>
2017-08-22 00:57:40 +0300

    Refactored transformers tests.

--- bidst/tests/test_transformers.py
+++ bidst/tests/test_transformers.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 import pytest
 
 import bidst
-from bidst.path_manager import PathManager
 from bidst.transformers import NUCorrectionTransformer
 from bidst.transformers import SkullStrippingTransformer
 from bidst.transformers import LinearRegistrationTransformer
@@ -19,39 +18,65 @@ from nipype.interfaces.ants import N4BiasFieldCorrection
 
 import nibabel as nib
 
-from sklearn.pipeline import Pipeline
-from nipype import Node, Workflow
-from shutil import rmtree
+
+def build_T1w_in_file_path(data_dir, subject, session):
+
+    return data_dir + \
+        '/sub-{}/ses-{}/anat/sub-{}_ses-{}_T1w.nii.gz'.format(
+            subject, session,
+            subject, session)
+
+
+def build_T1w_out_file_path(
+        data_dir,
+        pipeline_name,
+        subject,
+        session,
+        variant,
+        derivative):
+
+    full_path = '{}/derivatives/{}/' + \
+                'sub-{}/ses-{}/anat/' + \
+                'sub-{}_ses-{}_T1w_variant-{}_{}.nii.gz'
+
+    return full_path.format(data_dir,
+                            pipeline_name,
+                            subject, session,
+                            subject, session,
+                            variant,
+                            derivative)
 
 
 def test_skull_stripping_tuples():
 
     data_dir = os.path.abspath('.') + '/data/ds114'
-    pipeline_name = 'test_ROITransformer_ds114'
+    pipeline_name = 'test_SkullStrippingTransformer_ds114'
 
-    IDS = [('01', 'retest'), 
-           ('02', 'retest'), 
+    IDS = [('01', 'retest'),
+           ('02', 'retest'),
            ('03', 'test')]
 
-    transformer = SkullStrippingTransformer(data_dir=data_dir,
-                                            pipeline_name=pipeline_name,
-                                            search_param=dict(extensions='T1w.nii.gz'))
+    transformer = SkullStrippingTransformer(
+        data_dir=data_dir,
+        pipeline_name=pipeline_name,
+        search_param=dict(
+            extensions='T1w.nii.gz'),
+        variant='tuples')
     transformer.fit_transform(IDS)
-
-    PM = PathManager(data_dir)
 
     for subject, session in IDS:
 
-        in_file = PM.get(extensions='T1w.nii.gz',
-                         subject=subject,
-                         session=session)
+        in_file = build_T1w_in_file_path(data_dir,
+                                         subject,
+                                         session)
 
-        assert len(in_file) == 1
-        in_file = in_file[0]
+        out_file = build_T1w_out_file_path(data_dir,
+                                           pipeline_name,
+                                           subject,
+                                           session,
+                                           'tuples',
+                                           'needed_brain')
 
-        out_file = PM.make(in_file=in_file,
-                           pipeline_name=pipeline_name,
-                           derivative='needed_brain')
         dirname = os.path.dirname(out_file)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
@@ -60,41 +85,50 @@ def test_skull_stripping_tuples():
                      out_file=out_file)
         betfsl.run()
 
-        res_file = PM.make(in_file=in_file,
-                           pipeline_name=pipeline_name,
-                           derivative='brain')
+        res_file = build_T1w_out_file_path(data_dir,
+                                           pipeline_name,
+                                           subject,
+                                           session,
+                                           'tuples',
+                                           'brain')
 
         result_mat = nib.load(res_file).affine
         output_mat = nib.load(out_file).affine
         assert np.allclose(result_mat, output_mat)
-        os.remove(out_file)
         os.remove(res_file)
+        os.remove(out_file)
 
 
 def test_skull_stripping_ids_only():
 
     data_dir = os.path.abspath('.') + '/data/ds114'
-    pipeline_name = 'test_ROITransformer_ds114'
+    pipeline_name = 'test_SkullStrippingTransformer_ds114'
 
     IDS = ['01', '02', '03']
+    sessions = ['test', 'retest']
 
-    transformer = SkullStrippingTransformer(data_dir=data_dir,
-                                            pipeline_name=pipeline_name,
-                                            search_param=dict(extensions='T1w.nii.gz'))
+    transformer = SkullStrippingTransformer(
+        data_dir=data_dir,
+        pipeline_name=pipeline_name,
+        search_param=dict(
+            extensions='T1w.nii.gz'),
+        variant='ids_only')
     transformer.fit_transform(IDS)
 
-    PM = PathManager(data_dir)
-
     for subject in IDS:
+        for session in sessions:
 
-        in_files = PM.get(extensions='T1w.nii.gz',
-                         subject=subject)
-        
-        for in_file in in_files:
+            in_file = build_T1w_in_file_path(data_dir,
+                                             subject,
+                                             session)
 
-            out_file = PM.make(in_file=in_file,
-                               pipeline_name=pipeline_name,
-                               derivative='needed_brain')
+            out_file = build_T1w_out_file_path(data_dir,
+                                               pipeline_name,
+                                               subject,
+                                               session,
+                                               'ids_only',
+                                               'needed_brain')
+
             dirname = os.path.dirname(out_file)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
@@ -102,13 +136,16 @@ def test_skull_stripping_ids_only():
             betfsl = BET(in_file=in_file,
                          out_file=out_file)
             betfsl.run()
-
-            res_file = PM.make(in_file=in_file,
-                               pipeline_name=pipeline_name,
-                               derivative='brain')
+            
+            res_file = build_T1w_out_file_path(data_dir,
+                                               pipeline_name,
+                                               subject,
+                                               session,
+                                               'ids_only',
+                                               'brain')
 
             result_mat = nib.load(res_file).affine
             output_mat = nib.load(out_file).affine
             assert np.allclose(result_mat, output_mat)
-            os.remove(out_file)
             os.remove(res_file)
+            os.remove(out_file)
