5be7ee575f080050ce01097eb3730dbff15a7244
Dmitry Petrov <to.dmitry.petrov@gmail.com>
2017-08-30 00:33:38 -0700

    added project path to BIDSPipeline

--- bidst/pipeline.py
+++ bidst/pipeline.py
@@ -1,14 +1,16 @@
 from sklearn.pipeline import Pipeline
 
 class BIDSPipeline(Pipeline):
-    def __init__(self, steps, pipeline_name, memory=None):
+    def __init__(self, steps, pipeline_name, project_path, memory=None):
         super().__init__(steps)
         self.pipeline_name = pipeline_name
+        self.project_path = project_path
         # shallow copy of steps
         #self.steps = tosequence(steps)
         self._validate_steps()
         self._set_step_names()
         self._set_pipeline_name()
+        self._set_project_path()
 
 
     def _set_step_names(self):
@@ -20,3 +22,8 @@ class BIDSPipeline(Pipeline):
         for step in self.steps:
             #print(step)
             step[1].pipeline_name = self.pipeline_name
+
+    def _set_project_path(self):
+        for step in self.steps:
+            #print(step)
+            step[1].project_path = self.project_path
